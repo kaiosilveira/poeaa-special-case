bdf1e495d7efa31aed5de9697416cf16e3db81b3
Kaio Silveira <silveira.kaio@icloud.com>
2023-11-19 14:45:50 +0000

    add `GrossDailyRates` enum

---
+++ PoEAASpecialCase.Example/src/GrossDailyRates.cs
@@ -0,0 +1,7 @@
+namespace PoEAASpecialCase.Example.DailyRates;
+
+public static class GrossDailyRates
+{
+  public static readonly decimal PERMANENT = 1000m;
+  public static readonly decimal TEMPORARY = 800m;
+}
